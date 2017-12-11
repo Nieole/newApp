7a921bc1d98b663f68668821e3440f00db001839
Nicoer <saber00sword@gmail.com>
2017-12-11 17:35:50 +0800

    temp

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 	get 'users/new'
-	get  '/signup',  to: 'users#new'
+	get '/signup',  to: 'users#new'
 	get '/home',to: 'static_pages#home'
 	get '/contact',to: 'static_pages#contact'
 	get '/help', to: 'static_pages#help'
 	get '/about',to: 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "static_pages#home"
+	root "static_pages#home"
+	resources :users
 end
