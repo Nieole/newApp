1ba2bcad8b2f20c1cf6215be93437d7ee855509c
Nicoer <saber00sword@gmail.com>
2017-12-12 17:27:08 +0800

    8.22

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
 	get 'users/new'
 	get '/signup',  to: 'users#new'
 	post '/signup',  to: 'users#create'
@@ -6,6 +8,9 @@ Rails.application.routes.draw do
 	get '/contact',to: 'static_pages#contact'
 	get '/help', to: 'static_pages#help'
 	get '/about',to: 'static_pages#about'
+	get '/login',to: 'sessions#new'
+	post '/login',to: 'sessions#create'
+	delete '/logout',to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root "static_pages#home"
 	resources :users
