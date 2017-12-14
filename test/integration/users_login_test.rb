dee9d329690463473fa980cd3e1878e595166932
Nicoer <saber00sword@gmail.com>
2017-12-14 18:53:03 +0800

    Implement basic login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user=users(:michael)#通过users.yml获取配置好的信息
+  end
   # test "the truth" do
   #   assert true
   # end
@@ -12,5 +15,42 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+  test "login with invalid information2" do
+    get login_path
+    post login_path,params:{session:{email:@user.email,password:'password'}}
+    assert_redirected_to @user#检查重定向地址是否正确
+    follow_redirect!#访问重定向地址
+    assert_template 'users/show'
+    assert_select "a[href=?]",login_path,count:0
+    assert_select "a[href=?]",logout_path
+    assert_select "a[href=?]",user_path(@user)
+  end
+  test "Valid signup information" do
+    get signup_path
+    assert_difference 'User.count',1 do
+      post users_path,params:{user:{name:"Example User",email:"user@example.com",password:"password",password_confirmation:"password"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path,params:{session:{email:@user.email,password:'password'}}
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]",login_path,count:0
+    assert_select "a[href=?]",logout_path
+    assert_select "a[href=?]",user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "a[href=?]",login_path
+    assert_select "a[href=?]",logout_path,count:0
+    assert_select "a[href=?]",user_path(@user),count:0
   end
 end
